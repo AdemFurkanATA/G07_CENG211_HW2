4b06772a9b83ac0646cd139c40fb70b110fce924
ErkanArikan <erkan.arkn4@gmail.com>
2025-11-09 17:08:50 +0300

    Publication class yorumları düzenlendi son haline getirildi

--- src/model/Publication.java
+++ src/model/Publication.java
@@ -90,7 +90,6 @@ public class Publication {
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
-
         if (object == null || getClass() != object.getClass()) return false;
 
         Publication that = (Publication) object;
